095e048907f79a99c0d50e5c037294f48c061c04
Nikita Ilin <illinmikita@gmail.com>
2024-10-05 20:52:10 +0300

    push

--- js/index.js
+++ js/index.js
@@ -1,19 +1,37 @@
-
-const languageLinks = document.querySelectorAll('.language-selector');
 const userLang = navigator.language || navigator.userLanguage;
 
-// if (userLang.includes("es")) {
-//   changeLanguage('es');
-// } else if (userLang.includes("en")) {
-//     changeLanguage('en');
-// } 
 
-function changeLanguage(lang) {
-  if (lang === "en") {
-      window.location.href = "../en/";
-  } else if (lang === "sp") {
-      window.location.href = "../es/";
-  }
+if (userLang.includes("es")) {
+  changeLanguage('es');
+} else if (userLang.includes("en")) {
+  changeLanguage('en');
+}
+
+function changeLanguage(lang, event) {
+    // Проверяем, не находимся ли мы уже на странице выбранного языка
+    if (window.location.pathname.includes('/' + lang + '/')) {
+        console.log('Уже на странице выбранного языка');
+        return; // Выходим из функции, если уже на нужной странице
+    }
+
+    const selectors = document.querySelectorAll('.language-selector');
+    console.log('selectors', selectors);
+    selectors.forEach(selector => selector.classList.remove('active'));
+    
+    // Получаем нажатый элемент из события
+    const clickedElement = event.currentTarget;
+    console.log('clickedElement', clickedElement);
+    
+    // Добавляем класс 'active' к выбранному селектору
+    if (clickedElement) {
+        clickedElement.classList.add('active');
+    }
+    
+    // Устанавливаем новый язык
+    setLanguage(lang);
+    
+    // Перенаправляем на соответствующую страницу
+    window.location.href = '/' + lang + '/index.html';
 }
 
 function setLanguage(lang) {
@@ -33,6 +51,8 @@ function getLanguage() {
 }
 
 const savedLang = getLanguage();
-if (savedLang) {
+if (savedLang && !window.location.pathname.includes(savedLang)) {
     window.location.href = "/" + savedLang + "/";
 }
+
+console.log(savedLang)
