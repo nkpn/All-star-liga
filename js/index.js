ca7a0761d2e2b739bf0ec8c17ff46ec9183db996
Nikita Ilin <illinmikita@gmail.com>
2024-10-05 20:33:16 +0300

    both files are done

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const languageLinks = document.querySelectorAll('.language-selector');
-  const userLang = navigator.language || navigator.userLanguage;
 
-  // if (userLang.includes("es")) {
-  //   changeLanguage('es');
-  // } else if (userLang.includes("en")) {
-  //     changeLanguage('en');
-  // } 
+const languageLinks = document.querySelectorAll('.language-selector');
+const userLang = navigator.language || navigator.userLanguage;
 
-  function changeLanguage(lang) {
-    if (lang === "en") {
-        window.location.href = "../en/";
-    } else if (lang === "sp") {
-        window.location.href = "../es/";
-    }
+// if (userLang.includes("es")) {
+//   changeLanguage('es');
+// } else if (userLang.includes("en")) {
+//     changeLanguage('en');
+// } 
+
+function changeLanguage(lang) {
+  if (lang === "en") {
+      window.location.href = "../en/";
+  } else if (lang === "sp") {
+      window.location.href = "../es/";
   }
-});
+}
+
+function setLanguage(lang) {
+  document.cookie = "language=" + lang + "; path=/; max-age=" + (60 * 60 * 24 * 365); // 1 год
+  window.location.href = "/" + lang + "/";
+}
+
+function getLanguage() {
+    const cookies = document.cookie.split("; ");
+    for (let cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === "language") {
+            return value;
+        }
+    }
+    return null;
+}
+
+const savedLang = getLanguage();
+if (savedLang) {
+    window.location.href = "/" + savedLang + "/";
+}
